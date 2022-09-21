15ecf452aae629d129fca935be7267b6dabcb146
justinreligioso <jreligioso@hawk.iit.edu>
2022-09-20 21:03:32 -0500

    Add table to models file for Product_Upload

--- HawkShop/Product_Upload/models.py
+++ HawkShop/Product_Upload/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    productID = models.AutoField()
+    userID = models.AutoField()
+    price = models.DecimalField(decimal_places=2)
+    description = models.TextField()
+    condition = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(auto_now_add=True)
