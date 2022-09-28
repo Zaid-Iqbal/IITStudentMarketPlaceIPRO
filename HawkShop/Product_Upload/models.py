0fc63016bea08a91e4d7a68dcc7f10dd96cade82
Justin Religioso <89811196+justinreligioso@users.noreply.github.com>
2022-09-27 20:36:07 -0500

    Update models.py

--- HawkShop/Product_Upload/models.py
+++ HawkShop/Product_Upload/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     productID = models.AutoField()
+    productName = models.CharField(max_length=250)
     userID = models.AutoField()
     price = models.DecimalField(decimal_places=2)
     description = models.TextField()
