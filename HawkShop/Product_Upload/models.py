5a8a6cd90ebd06670ea7d7f78630b57e3f6ed021
justinreligioso <jreligioso@hawk.iit.edu>
2022-10-04 18:48:09 -0500

    makemigrations ran

--- HawkShop/Product_Upload/models.py
+++ HawkShop/Product_Upload/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+class User(models.Model):
+    user = models.IntegerField()
+    name = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 100)
+    phoneNumber = models.IntegerField()
+    password = models.CharField(max_length=100)
+    creditCard = models.IntegerField()
+
 class Product(models.Model):
-    productID = models.AutoField()
+    product = models.IntegerField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
     productName = models.CharField(max_length=250)
-    userID = models.ForeignKey('User', on_delete=models.CASCADE)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
     condition = models.CharField(max_length=100)
     date_posted = models.DateTimeField(auto_now_add=True)
--- HawkShop/User_Registration/models.py
+++ HawkShop/User_Registration/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class User(models.Model):
-    userID = models.AutoFiled()
+    user = models.IntegerField()
     name = models.CharField(max_length = 100)
     email = models.CharField(max_length = 100)
     phoneNumber = models.IntegerField()
     password = models.CharField(max_length=100)
     creditCard = models.IntegerField()
+
+class Product(models.Model):
+    product = models.IntegerField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    productName = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    condition = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    sold = models.BooleanField()
