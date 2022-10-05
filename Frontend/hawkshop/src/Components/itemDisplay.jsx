a1b80f1b16e6f2b2217ef2e06e35022dfa723664
Parth Patel <parth.b.patel5@gmail.com>
2022-10-04 20:01:39 -0500

    made demo search

--- Frontend/hawkshop/src/Components/itemDisplay.jsx
+++ Frontend/hawkshop/src/Components/itemDisplay.jsx
@@ -4,18 +4,19 @@ import {Component} from "react";
 import Item from "./item";
 
 class ItemDisplay extends Component{
+    /*
     state ={
         items : [
             {id:'1230', name:'Hi', price:23, condition:'Bad' },
             {id:'1231', name:'Hello', price:450, condition:'Fair' },
             {id:'1232', name:'Bye', price:203, condition:'Good' }
         ]
-    }
+    }*/
 
     render() {
         return(
             <div class='p-2'>
-                {this.state.items.map(item => (
+                {this.props.items.map(item => (
                     <Item key = {item.id}
                           name = {item.name}
                           price = {item.price}
--- Frontend/hawkshop/src/Components/search.jsx
+++ Frontend/hawkshop/src/Components/search.jsx
@@ -1,24 +1,60 @@
 import {Component} from "react";
 import ItemDisplay from "./itemDisplay";
+import item from "./item";
 
 
 class Search extends Component{
     constructor() {
         super();
-        this.state = {value: ''};
+        this.state = {value: '',
+                orgitems:[
+                    {id:'1230', name:'Laptop', price:23, condition:'Bad' },
+                    {id:'1231', name:'English Book', price:450, condition:'Fair' },
+                    {id:'1232', name:'Fridge', price:203, condition:'Good' },
+                    {id:'1233', name:'Brand New Fridge', price:23, condition:'Bad' },
+                    {id:'1234', name:'Math 251 Book', price:450, condition:'Fair' },
+                    {id:'1235', name:'TI-84 Calculator', price:203, condition:'Good' },
+                    {id:'1236', name:'MacBook Laptop', price:23, condition:'Bad' },
+                    {id:'1237', name:'Samsung Fridge', price:450, condition:'Fair' },
+                    {id:'1238', name:'Vaccum', price:203, condition:'Good' },
+                    {id:'1239', name:'Small Rug', price:23, condition:'Bad' },
+                    {id:'1240', name:'Hello', price:450, condition:'Fair' },
+                    {id:'1241', name:'Dyson Vaccum', price:203, condition:'Good' },
+                    {id:'1242', name:'Big Rug', price:23, condition:'Bad' },
+                    {id:'1243', name:'CS 351 book', price:450, condition:'Fair' },
+                    {id:'1244', name:'HP Laptop', price:203, condition:'Good' },
+                    {id:'1245', name:'Basic Calculator', price:23, condition:'Bad' },
+                    {id:'1246', name:'Dell Laptop', price:450, condition:'Fair' },
+                    {id:'1247', name:'Rug', price:203, condition:'Good' },
+                ],
+                items:[]
+
+                    };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        console.log(this.state.value);
         this.setState({value: event.target.value});
     }
 
     handleSubmit(event) {
         console.log(this.state.value);
         alert('A name was submitted: ' + this.state.value);// instead of this we send this.state.val to back
-        event.preventDefault();
+
+        // this was demo search
+        const items = [];
+        for (let i = 0; i<this.state.orgitems.length; i++){
+            if (this.state.orgitems[i].name.toLowerCase().includes(this.state.value.toLowerCase())){
+                console.log(this.state.orgitems[i].name)
+                items.push(this.state.orgitems[i]);
+            }
+        }
+
+        this.setState({items});
+
+        // end demo
+        event.preventDefault(); // recieve data, and then pass in data
     }
 
     render() {
@@ -32,7 +68,7 @@ class Search extends Component{
                            aria-describedby="search-addon" onChange={this.handleChange}/>
                     <button onClick={this.handleSubmit} type="button" className="btn btn-outline-dark">search</button>
                 </div>
-                <ItemDisplay></ItemDisplay>
+                <ItemDisplay items={this.state.items}></ItemDisplay>
             </div>
         );
     }
