b9548a64fb7941e743722f4c5ba568fcd3906f89
justinreligioso <jreligioso@hawk.iit.edu>
2022-11-01 20:53:18 -0500

    updated connection

--- React&Node/hawkshopapp/src/Components/demo_db_connection.js
+++ React&Node/hawkshopapp/src/Components/demo_db_connection.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
+  host: "hawkshop-database.crubpolzuyub.us-east-2.rds.amazonaws.com",
+  user: "jreligioso",
+  password: "3MysLzX6Qv9US3Quz2E4", 
+  port: '3306', 
+  database: 'hawkshop-database'
 });
 
 con.connect(function(err) {
