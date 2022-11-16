17ff3f3f166e4e2f54e303037f17653b4ca79bb6
Samruddhi <skakade2@hawk.iit.edu>
2022-11-15 18:32:13 -0600

    Test

--- ReactNode/hawkshopapp/src/Components/demo_db_connection.js
+++ ReactNode/hawkshopapp/src/Components/demo_db_connection.js
@@ -4,8 +4,7 @@ var con = mysql.createConnection({
   host: "hs-db.crubpolzuyub.us-east-2.rds.amazonaws.com",
   user: "admin",
   password: "password", 
-  port: '3306', 
-  
+  port: '3306'
 });
 
 con.connect(function(err) {
